3c7f4dd0a450da9794f2ef19b2e013d460094cb2
Luana Ribeiro <luanasribeiro94@gmail.com>
2019-11-15 09:36:07 -0300

    chore: criação do projeto

---
+++ app/src/main/java/com/example/trabalhodesenvolvimentomovel/ListagemEmprestimos.java
@@ -0,0 +1,14 @@
+package com.example.trabalhodesenvolvimentomovel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListagemEmprestimos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listagem_emprestimos);
+    }
+}
