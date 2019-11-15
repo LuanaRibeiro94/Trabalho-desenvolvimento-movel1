131d4c6d47654ef3e6a37fbbd7dc12c24984c1eb
Luana Ribeiro <luanasribeiro94@gmail.com>
2019-11-15 10:02:03 -0300

    feat: implementa opção menuFecharApp

--- app/src/main/java/com/example/trabalhodesenvolvimentomovel/ListagemEmprestimos.java
+++ app/src/main/java/com/example/trabalhodesenvolvimentomovel/ListagemEmprestimos.java
@@ -1,9 +1,11 @@
 package com.example.trabalhodesenvolvimentomovel;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class ListagemEmprestimos extends AppCompatActivity {
 
@@ -17,5 +19,16 @@ public class ListagemEmprestimos extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_listagem_emprestimos, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.menuFecharApp: {
+                finish();
+                break;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
