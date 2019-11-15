74edd62c921811c05b055796a1644ac487f12e72
Luana Ribeiro <luanasribeiro94@gmail.com>
2019-11-15 09:54:28 -0300

    feat: adiciona menu com as opções: cliente, livro, Empréstimo e fechar

--- app/src/main/java/com/example/trabalhodesenvolvimentomovel/ListagemEmprestimos.java
+++ app/src/main/java/com/example/trabalhodesenvolvimentomovel/ListagemEmprestimos.java
@@ -3,6 +3,7 @@ package com.example.trabalhodesenvolvimentomovel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class ListagemEmprestimos extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class ListagemEmprestimos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listagem_emprestimos);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_listagem_emprestimos, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
