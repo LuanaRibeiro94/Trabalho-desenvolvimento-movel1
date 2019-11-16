db334523a7682bd80262241346b333728dee9274
Luana Ribeiro <luanasribeiro94@gmail.com>
2019-11-16 09:27:02 -0300

    feat: adiciona activity FormLivro para CRUD de livros

--- app/src/main/java/com/example/trabalhodesenvolvimentomovel/ListagemLivros.java
+++ app/src/main/java/com/example/trabalhodesenvolvimentomovel/ListagemLivros.java
@@ -1,13 +1,28 @@
 package com.example.trabalhodesenvolvimentomovel;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ListagemLivros extends AppCompatActivity {
+    Button btnNovoCadastro;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listagem_livros);
+
+        btnNovoCadastro = findViewById(R.id.btnNovoCadastro);
+
+        btnNovoCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(ListagemLivros.this, FormLivro.class);
+                startActivity(i);
+            }
+        });
     }
 }
